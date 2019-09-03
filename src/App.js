d40bf83a0614c5cecd72d78441aa1f7f67f078c0
nikoosokhan <nikoosokhan.fakher@gmail.com>
2019-09-03 19:51:26 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Timer from "./Timer";
+
+class App extends React.Component{
+    // constructor(props){
+    //     super(props);
+    // }
+
+    //methods
+
+    render() {
+        return(
+            <Timer/>
+        )
+    }
+}
+
+export default App;
---
+++ src/Timer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Timer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            time: 5
+        }
+    }
+    handleClick(){
+        this.setState({time : this.state.time*3600});
+
+        setInterval(()=>{
+            this.setState({time : this.state.time-1});
+            }
+        ,1000)
+    }
+
+    render() {
+        return(
+            <div>
+                {()=>this.handleClick()}
+                <div>
+                    {   parseInt(this.state.time/3600%24)+ ':' +
+                        parseInt(this.state.time/60%60)+ ':' +
+                        parseInt(this.state.time%60)
+                    }
+                </div>
+                {/*<div>*/}
+                {/*    <button onClick={}>click me</button>*/}
+                {/*</div>*/}
+            </div>
+        )
+    }
+}
+
+export default Timer;
